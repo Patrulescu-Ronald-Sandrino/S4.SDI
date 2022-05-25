79dd465135682f903ff3ffd0ea029af80362bfda
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-05-25 20:37:36 +0300

    [A11] [web] add Docket to SwaggerConfig

--- lab11/web/src/main/java/ro/lab11/web/config/SwaggerConfig.java
+++ lab11/web/src/main/java/ro/lab11/web/config/SwaggerConfig.java
@@ -1,8 +1,18 @@
 package ro.lab11.web.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 // https://www.concretepage.com/spring-4/spring-rest-swagger-2-example
 // http://localhost:8080/api/v2/api-docs
@@ -10,7 +20,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Bean
+    public Docket customImplementation(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ro.lab11.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("REST Api Documentation",
+                "REST Api Documentation",
+                "1.0",
+                "urn:tos",
+                new Contact("Patrulescu Sandrino", "https://facebook.com/sandrinopatrulescu", "sandrinopatrulescu@yahoo.com"),
+                "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
 }
 
 
