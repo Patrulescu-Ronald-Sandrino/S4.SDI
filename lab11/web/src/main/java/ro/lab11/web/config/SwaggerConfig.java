fa4afb346085a4a2a615e6bd7980d1bc8971bc71
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-05-25 20:29:04 +0300

    [A11] [web] configure swagger

---
+++ lab11/web/src/main/java/ro/lab11/web/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package ro.lab11.web.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+// https://www.concretepage.com/spring-4/spring-rest-swagger-2-example
+// http://localhost:8080/api/v2/api-docs
+// http://localhost:8080/api/swagger-ui.html
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+}
+
+
--- lab11/web/src/main/java/ro/lab11/web/config/WebConfig.java
+++ lab11/web/src/main/java/ro/lab11/web/config/WebConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ro.lab11.web.controller.Controller;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"ro.lab11.web.controller", "ro.lab11.web.converter"})
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -23,5 +24,12 @@ public class WebConfig {
                         .allowedMethods("GET", "PUT", "POST", "DELETE");
             }
         };
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
